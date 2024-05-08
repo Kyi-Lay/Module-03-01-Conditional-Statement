b4b71f251b3de493580287c0afdbb695e0928d21
pannajota <jotapanna2014@gmail.com>
2024-05-08 14:02:38 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = x < 25;
+var expression2 = x > 50;
+
+// TODO: Write Your JavaScript Code Here
+switch (true) {
+    case x == 50:
+        console.log('True and True')
+        break;
+    case x < 60:
+        console.log('true and true');
+        break;
+    case x > 45:
+        console,log('flase and true');
+        break;
+    default:
+        console.log('flase and flase')
+}
